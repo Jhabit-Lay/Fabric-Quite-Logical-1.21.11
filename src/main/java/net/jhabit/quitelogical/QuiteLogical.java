3562236296bd9fae60c68d2d442559413f6ad9e4
Jhabit-Lay <znsl930@gmail.com>
2026-01-16 00:02:34 +0900

    Initial commit

---
+++ src/main/java/net/jhabit/quitelogical/QuiteLogical.java
@@ -0,0 +1,17 @@
+package net.jhabit.quitelogical;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QuiteLogical implements ModInitializer {
+	public static final String MOD_ID = "quite-logical";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jhabit/quitelogical/QuiteLogicalClient.java
@@ -0,0 +1,10 @@
+package net.jhabit.quitelogical;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QuiteLogicalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
