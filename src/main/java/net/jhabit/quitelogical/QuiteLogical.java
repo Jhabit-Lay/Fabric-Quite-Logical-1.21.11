9ec26f07b717ee7a8424ce3058edf053e3059f4d
Jhabit-Lay <znsl930@gmail.com>
2026-01-19 21:49:07 +0900

    RESIN_CLUMP can now wax!

--- src/main/java/net/jhabit/quitelogical/QuiteLogical.java
+++ src/main/java/net/jhabit/quitelogical/QuiteLogical.java
@@ -1,20 +1,27 @@
 package net.jhabit.quitelogical;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
 import net.jhabit.quitelogical.block.ModBlocks;
 import net.jhabit.quitelogical.items.ModItems;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.cow.Cow;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.HoneycombItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.state.BlockState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 public class QuiteLogical implements ModInitializer {
 	public static final String MOD_ID = "qlogic";
@@ -36,6 +43,31 @@ public class QuiteLogical implements ModInitializer {
 
 		FabricDefaultAttributeRegistry.register(EntityType.COW, Cow.createAttributes().add(Attributes.ATTACK_DAMAGE, 2.0));
 		FabricDefaultAttributeRegistry.register(EntityType.MOOSHROOM, Cow.createAttributes().add(Attributes.ATTACK_DAMAGE, 2.0));
+
+		// [레진 밀랍칠 기능 구현]
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+			ItemStack stack = player.getItemInHand(hand);
+
+			if (stack.is(Items.RESIN_CLUMP)) {
+				var pos = hitResult.getBlockPos();
+				BlockState state = world.getBlockState(pos);
+
+				Optional<BlockState> waxedState = HoneycombItem.getWaxed(state);
+
+				if (waxedState.isPresent()) {
+					if (!world.isClientSide()) {
+						world.setBlock(pos, waxedState.get(), 11);
+						world.levelEvent(null, 3003, pos, 0);
+
+						if (!player.isCreative()) {
+							stack.shrink(1);
+						}
+					}
+					return InteractionResult.SUCCESS;
+				}
+			}
+			return InteractionResult.PASS;
+		});
 
 		LOGGER.info("Quite Logical (Mojmap) 초기화 완료!");
 	}
